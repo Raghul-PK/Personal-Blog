88dc11298e436ee9147c2d1b9015fe6dd9bd1f18
Raghul PK <pkraghul2001@gmail.com>
2024-04-08 22:10:34 +0530

    added new stuff

--- backend/server.py
+++ backend/server.py
@@ -9,27 +9,29 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Sample posts data
-posts = [
-    {"id": 1, "title": "Post 1", "content": "Content of post 1", "date": "2024-04-08"},
-    {"id": 2, "title": "Post 2", "content": "Content of post 2", "date": "2024-04-09"},
-    # Add more posts here as needed
-]
-
 # Route to serve posts data
 @app.route('/posts')
 def get_posts():
-    return jsonify(posts)
+    print(os.listdir("./posts"))
+    all_posts = []
+    for f in os.listdir("./posts"):
+        file_name = "./posts/" + f
+        with open(file_name, "r") as file:
+            data = json.load(file)
+        all_posts.append(data)
+    print(all_posts)
+    return jsonify(all_posts)
 
 @app.route('/addpost', methods=['POST'])
 def create_post():
     data = request.json
     current_datetime = datetime.now()
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    data['time'] = formatted_datetime
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+    data['date'] = formatted_datetime
     print(data)
     # Write JSON data to a file
-    with open("posts/data.json", "w") as file:
+    file_name = "posts/" + data['date'] + ".json"
+    with open(file_name, "w") as file:
         json.dump(data, file)
     return jsonify({'message': 'Post created successfully'}), 201
 
